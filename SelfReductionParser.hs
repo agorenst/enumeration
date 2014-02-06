66a3cf134942b63250db3cb794ee404dd929ed97
agorenst <agorenst@cs.wisc.edu>
2014-02-06 14:02:57 -0600

    Added separate program, this one for self-reducibility of NP-hard problems

---
+++ SelfReductionParser.hs
@@ -0,0 +1,13 @@
+module SelfReductionParser where
+
+stringToCNF :: String -> [[Int]]
+stringToCNF str =
+    let ls = lines str -- each line is at most 1 clause
+        split = (map words ls) -- parse around whitespace in each line
+        nonzero = map (filter (\x -> x /= "0")) split -- rid of all zeros
+        nonempt = filter (\l -> length l >= 1) nonzero -- get rid of whitespace only lines
+        clauses = filter (\l -> head l /= "c" && head l /= "p") nonempt -- ignore comments, problem spec
+    in map (map readInt) clauses
+
+readInt :: String -> Int
+readInt = read
